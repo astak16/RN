b5e514f05bb5fae7c72d9c04b2915df0b3168247
tiantian <1500846601@qq.com>
2020-01-10 11:35:33 +0800

    自定义中间件

--- js/store/index.js
+++ js/store/index.js
@@ -2,7 +2,19 @@ import {applyMiddleware,createStore} from "redux"
 import thunk from "redux-thunk"
 import reducers from "../renducer"
 
+const logger = store => next => action => {
+  if(typeof action === "function"){
+    console.log("dispatching a function")
+  }else{
+    console.log("dispatching", action)
+  }
+  const result = next(action)
+  console.log("nextState",store.getState())
+  return result
+}
+
 const middlewares = [
+  logger,
   thunk
 ]
 
