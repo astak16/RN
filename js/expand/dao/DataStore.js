269e4eeeb602591710e4a4243183b4bc7f1222b3
tiantian <1500846601@qq.com>
2020-01-11 00:16:02 +0800

    实现离线缓存框架

---
+++ js/expand/dao/DataStore.js
@@ -0,0 +1,109 @@
+import {AsyncStore} from "react-native"
+
+export default class DataStore {
+  /**
+   * 获取数据，优先获取本地数据，如果无本地数据或本地数据过期咋获取网络数据
+   * @param url
+   * @returns {Promise<R>}
+   */
+  fetchData(url) {
+    return new Promise((resolve, reject) => {
+      this.fetchData(url)
+        .then(wrapData => {
+          if (wrapData && DataStore.checkTimestampValid(wrapData.timestamp)) {
+            resolve(wrapData)
+          } else {
+            this.fetchData(url).then(data => {
+              resolve(this._wrapData(data))
+            }).catch(error => {
+              reject(error)
+            })
+          }
+        }).catch(error => {
+        this.fetchData(url).then(data => {
+          resolve(this._wrapData(data))
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    })
+  }
+  
+  /**
+   * 保存数据
+   * @param url
+   * @param data
+   * @param callback
+   */
+  saveData(url, data, callback) {
+    if (!data || !url) return
+    AsyncStore.setItem(url, JSON.stringify(this._wrapData(data)), callback)
+  }
+  
+  /**
+   * 获取本地数据
+   * @param url
+   * @returns {Promise<R>}
+   */
+  fetchLocalData(url) {
+    return new Promise((resolve, reject) => {
+      AsyncStore.getItem(url, (error, result) => {
+        if (!error) {
+          try {
+            resolve(JSON.parse(result))
+          } catch (e) {
+            reject(e);
+            console.error(e)
+          }
+        } else {
+          reject(error)
+          console.error(error)
+        }
+      })
+    })
+  }
+  
+  /**
+   * 获取网络数据
+   * @param url
+   * @returns {Promise<R>}
+   */
+  fetchNetData(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(response => {
+          if (response.ok) {
+            return response.json()
+          }
+          throw new Error("Network response was not ok.")
+        })
+        .then(responseData => {
+          this.saveData(url, responseData)
+          resolve(responseData)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  }
+  
+  _wrapData(data) {
+    return {data: data, timestamp: new Date().getTime}
+  }
+  
+  /**
+   * 检查timestamp是否在有效期内
+   * @param timestamp
+   * @returns {boolean}
+   */
+  static checkTimestampValid(timestamp) {
+    const currentDate = new Date()
+    const targetDate = new Date()
+    targetDate.setTime(timestamp)
+    if (currentDate.getMonth() !== targetDate.getMonth()) return false
+    if (currentDate.getDate() !== targetDate.getDate()) return false
+    if (currentDate.getHours() - targetDate.getHours() > 4) return false
+    return true
+  }
+  
+}
